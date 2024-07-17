17e7313c1b50c22c1d310338cc0ed0c208df3cb2
Sichen <sichen@quatt.io>
2024-07-17 10:31:18 +0200

    update CommonQuery

--- Mysql/connectMysql.py
+++ Mysql/connectMysql.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 from .loadMysqlCredentials import mysql_host, mysql_user, mysql_password, mysql_port, mysql_database
-
+from . import CommonQuery
 
 def connect_mysql(arguments):
 
@@ -11,22 +11,27 @@ def connect_mysql(arguments):
                                              password = mysql_password,
                                              database = mysql_database,
                                              port     = mysql_port)
+
         if connection.is_connected():
             db_Info = connection.get_server_info()
             print("Connected to MySQL Server version ", db_Info)
             cursor = connection.cursor()
-            #cursor.execute("SELECT * FROM cic;")
-            #fields = [field_md[0] for field_md in cursor.description]
-            #result = [dict(zip(fields,row)) for row in cursor.fetchall()]
-            #print(result)
+
+            #CommonQuery.ListDataBases(cursor)
+            #CommonQuery.ListTables(cursor)
+            #CommonQuery.MartijnCO2Calculation(cursor) 
+            #CommonQuery.testQuery(cursor)
+            CommonQuery.test2Query(cursor)
+
     except Error as e:
             print("error out.")
+    '''
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
             print("MySQL connection is closed")
-
+    '''
 
 
 
